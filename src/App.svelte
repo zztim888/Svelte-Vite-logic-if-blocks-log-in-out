0070e1e4e2a95d8e2326fe94e3e5e48edbeaeb75
Otim <53155760+zztim888@users.noreply.github.com>
2022-11-02 22:54:24 +0100

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+ let user = {loggedIn:false};
+
+  function toggle(){
+    user.loggedIn = !user.loggedIn;
+  }
+</script>
+
+<h1>Log in and log out</h1>
+
+<!-- if logged in you can log out-->
+{#if user.loggedIn}
+<button on:click={toggle}>
+  Log Out
+</button>
+{/if}
+
+<!-- if logged out you can log in-->
+{#if !user.loggedIn}
+<button on:click={toggle}>
+  Log In
+</button>
+{/if}
